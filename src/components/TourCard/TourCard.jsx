d60aa8174b9c3dc5cd86938dd7332a2882ebf839
MANISHA <manishapanchal169@gmail.com>
2022-07-11 13:47:09 +0530

    import card comp. in tour card comp. ane fix typo

--- src/components/TourCard/TourCard.jsx
+++ src/components/TourCard/TourCard.jsx
@@ -1,11 +1,13 @@
-import React,{Components} from 'react';
-
-class TourCard extends React.Component {
-
-render(){
-  return(
-    <div>hii</div>
-  )
-}
+import React, { Component } from "react";
+import Card from "../Card/Card";
+class TourCard extends Component {
+  render() {
+    return (
+      <div>
+        <div>Our Tour</div>
+        <Card />
+      </div>
+    );
+  }
 }
 export default TourCard;
