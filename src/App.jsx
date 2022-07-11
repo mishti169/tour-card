124f5e9b122e4649a3ef137c20529ae22a7ab7d8
MANISHA <manishapanchal169@gmail.com>
2022-07-11 11:16:10 +0530

    make app class base component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-// import { useState } from 'react'
+import React , { Component} from 'react'
 import './App.css'
 
-function App() {
 
-  return (
-    <div className="App">
-    </div>
-  )
+class  App extends Component {
+
+  render(){
+    return (
+      <div>
+        <TourCard/>
+      </div>
+    )
+  }
 }
 
-export default App
-e
+export default App;
