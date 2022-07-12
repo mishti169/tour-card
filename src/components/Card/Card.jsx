ee1daf0bc2658b512cd66bd234a24b7792a8cd6e
MANISHA <manishapanchal169@gmail.com>
2022-07-12 16:28:04 +0530

    remove api data handling and map function from card comp

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,36 +1,22 @@
 import React, { Component } from "react";
-import axios from "axios";
 
 class Card extends Component {
-  state = {
-    data: [],
-  };
-  async getTourInfo() {
-    const { data } = await axios.get(
-      "https://course-api.com/react-tours-project"
-    );
-    this.setState({ data });
-  }
-  componentDidMount() {
-    this.getTourInfo();
-  }
-
   render() {
-    return this.state.data.map(function (item) {
-      return (
+    const { item } = this.props;
+
+    return (
+      <div>
+        <img width={620} height={320} src={item.image} alt={item.name} />
         <div>
-          <img width={620} height={320} src={item.image} alt={item.name} />
           <div>
-            <div>
-              <span>{item.name}</span>
-              <span>{item.price}</span>
-            </div>
-            <p>{item.info}</p>
-            <span>Not Interested</span>
+            <span>{item.name}</span>
+            <span>{item.price}</span>
           </div>
+          <p>{item.info}</p>
+          <span>Not Interested</span>
         </div>
-      );
-    });
+      </div>
+    );
   }
 }
 export default Card;
