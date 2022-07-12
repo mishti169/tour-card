a848cbbfb18a6c45fc270582ea6ef97fe70caa5e
MANISHA <manishapanchal169@gmail.com>
2022-07-12 16:29:07 +0530

    add api data handling function and map function to tour-card comp

--- src/components/TourCard/TourCard.jsx
+++ src/components/TourCard/TourCard.jsx
@@ -1,11 +1,29 @@
 import React, { Component } from "react";
 import Card from "../Card/Card";
+import axios from "axios";
+
 class TourCard extends Component {
+  state = {
+    data: [],
+  };
+  async getTourInfo() {
+    const { data } = await axios.get(
+      "https://course-api.com/react-tours-project"
+    );
+    this.setState({ data });
+  }
+  componentDidMount() {
+    this.getTourInfo();
+  }
+
   render() {
     return (
       <div>
         <div>Our Tour</div>
-        <Card />
+        {["1", 1]}
+        {this.state.data.map(function (currItem) {
+          return <Card item={currItem} />;
+        })}
       </div>
     );
   }
